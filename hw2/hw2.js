d1d074bbfd31cf7eedf63d66c84e586ba350fda7
Yumin Chen <bk201@YumindeMac-Pro.local>
2018-10-09 16:36:36 -0700

    HW2 DEMO Fixed

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -187,7 +187,7 @@ $('#ipInfo').submit(function (event) {
     }else {
         var ansMin = minArr[0]+'.'+minArr[1]+'.'+minArr[2]+'.'+minArr[3];
         var ansMax = maxArr[0]+'.'+maxArr[1]+'.'+maxArr[2]+'.'+maxArr[3];
-        
+        $("#resultReturn").empty();
         $('#resultReturn').html(
             '<h5 class="mt-5">Network: '+ansMin+'/'+mask+'</h5>'+
             '<h5>Min Address: '+ansMin+'(Network)</h5>'+
